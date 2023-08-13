d6e3ec17f0131b5c35e62394d12f021a152a1978
Tim Brockley <timbrockley@gmail.com>
2023-08-13 16:19:39 +0100

    initial commit

---
+++ _archive/index-street-portraits.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en-GB">
+
+  <head>
+
+    <meta http-equiv="content-type" content="text/html; charset=utf-8">
+
+    <meta http-equiv="refresh" content="0; URL=https://www.timbrockley.co.uk/street-portraits.html">
+
+    <link rel="canonical" href="https://www.timbrockley.co.uk/street-portraits.html">
+
+    <title>Street Portraits - Tim Brockley Photography</title>
+
+  </head>
+
+  <body>
+
+    <p>(301) The requested page has moved permanantly.</p>
+    <p><a href="https://www.timbrockley.co.uk/street-portraits.html">https://www.timbrockley.co.uk/street-portraits.html</a></p>
+
+  </body>
+
+</html>
